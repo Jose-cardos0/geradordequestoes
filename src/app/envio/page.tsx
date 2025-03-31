95f5ca9923f8e47220004daec0184ba84063f9d0
Jose <josedeveloperJS@gmail.com>
2025-03-31 17:59:28 -0300

    first submit

---
+++ src/app/envio/page.tsx
@@ -0,0 +1,10 @@
+export default function Enviar() {
+  return (
+    <main
+      className="h-screen flex items-center justify-center bg-gradient-to-br 
+       from-purple-900 via-black to-black"
+    >
+      envio
+    </main>
+  );
+}
