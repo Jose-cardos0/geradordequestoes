d47635a7438e0a476cf7bbb837bf611520ccaada
Jose <josedeveloperJS@gmail.com>
2025-04-09 00:02:10 -0300

    correcoes finais

--- src/app/envio/page.tsx
+++ src/app/envio/page.tsx
@@ -17,12 +17,12 @@ import { useState } from "react";
 
 export default function Enviar() {
   const [banca, setBanca] = useState("");
-  const [pdf, setPdf] = useState<any>();
+  const [pdf, setPdf] = useState<any | string>();
   const [nivel, setNivel] = useState("");
   const [questoes, setQuestoes] = useState("");
   const [loading, setLoading] = useState(false);
-
-  // const [arquivoGerado, setArquivo] = useState<string | any>();
+  const [resultado, setResultado] = useState<boolean>(false);
+  const [arquivoTexto, setArquivo] = useState<string | any>();
 
   const prompt = `utilize este arquivo em ${pdf} para criar
    10 questões sobre o conteúdo contido dentro do arquivo. Me retorne questões sobre o assunto. 
@@ -50,31 +50,55 @@ export default function Enviar() {
         contents: [part, userContent],
       });
 
-      // setArquivo(response.text);
-      const arquivoTexto: string | any = response.text;
+      const arquivosData: string | any = response.text;
+      setArquivo(arquivosData);
       setLoading(false);
+      setResultado(true);
       console.log(arquivoTexto);
 
-      const pdf = new jsPDF();
-      pdf.setFontSize(9);
+      // const pdf = new jsPDF();
+      // pdf.setFontSize(9);
 
-      // Adicionar o texto ao PDF em várias páginas
-      const linhas = pdf.splitTextToSize(arquivoTexto, 170); // largura máxima em pontos
-      let y = 10;
-      for (const linha of linhas) {
-        if (y > 250) {
-          pdf.addPage();
-          y = 10;
-        }
-        pdf.text(linha, 15, y);
-        y += 5;
-      }
+      // const linhas = pdf.splitTextToSize(arquivoTexto, 170);
+      // let y = 10;
+      // for (const linha of linhas) {
+      //   if (y > 250) {
+      //     pdf.addPage();
+      //     y = 10;
+      //   }
+      //   pdf.text(linha, 15, y);
+      //   y += 5;
+      // }
 
-      const filename = "questões.pdf"; // Nome do arquivo PDF
-      pdf.save(filename);
+      // const filename = "questões.pdf";
+      // pdf.save(filename);
     }
 
     main();
+  }
+
+  function getPdf() {
+    if (!arquivoTexto) {
+      console.error("O texto do arquivo não está definido.");
+      return;
+    }
+
+    const pdf = new jsPDF();
+    pdf.setFontSize(9);
+
+    // Adicionar o texto ao PDF em várias páginas
+    const linhas = pdf.splitTextToSize(arquivoTexto, 170); // largura máxima em pontos
+    let y = 10;
+    for (const linha of linhas) {
+      if (y > 250) {
+        pdf.addPage();
+        y = 10;
+      }
+      pdf.text(linha, 15, y);
+      y += 5;
+    }
+    const filename = "questões.pdf";
+    pdf.save(filename);
   }
 
   return (
@@ -99,6 +123,59 @@ export default function Enviar() {
             <br />
             os dados.
           </p>
+        </section>
+      )}
+      {/*Resultado do texto*/}
+      {resultado && (
+        <section
+          id="loading"
+          className="absolute z-100
+     h-screen  w-screen 
+      bg-gradient-to-br flex flex-col items-center 
+      justify-center m-auto
+      from-purple-900 via-black to-black "
+        >
+          <section className="flex-col items-center pt-4 relative ">
+            {/* <div className="absolute z-50 -rotate-20 ">
+          <Image src="/florpng.png" alt="Logo" width={35} height={35} />
+        </div> */}
+            <div className="flex items-center justify-center m-auto mb-4">
+              <div className="spinner ">
+                <div className="universe"></div>
+              </div>
+              <div>
+                <p className=" font-bold text-lg ml-4 animate-pulse text-orange-500">
+                  CLICK
+                </p>
+                <p
+                  className=" font-bold text-lg ml-4 animate-pulse
+            -mt-2 text-purple-700"
+                >
+                  EM DOWNLOAD
+                </p>
+              </div>
+            </div>
+            <div className="">
+              <h1
+                className="text-white ml-8 text-center mb-4
+          text-xs mr-8 font-bold drop-shadow-lg"
+              >
+                <strong>AutoQuest</strong>
+                <p>Criação automática de questões.</p>
+              </h1>
+            </div>
+          </section>
+          <button
+            onClick={getPdf}
+            id="donwload"
+            className="bg-gradient-to-tl to-orange-600
+             via-purple-400 from-purple-800 p-2
+              cursor-pointer font-bold text-white
+               bg-animate
+              rounded-2xl px-8 hover:scale-105 duration-300 transform "
+          >
+            Download
+          </button>
         </section>
       )}
       <section className="flex items-center ml-4 pt-4 relative ">
@@ -138,22 +215,33 @@ export default function Enviar() {
           onSubmit={handleSubmit}
           className="w-1/2 px-16 max-md:px-8 max-md:w-screen"
         >
-          <p className="text-xs mt-4">Selecione um arquivo em pdf</p>
-          <input
-            onChange={(e) => {
-              if (e.target.files) {
-                setPdf(e.target.files[0]);
-              }
-            }}
-            type="file"
-            accept=".pdf"
-            className="block w-full text-sm text-gray-300
+          <div className="border p-4 rounded-lg border-[#1a1a1a]">
+            <p className="text-xs mt-4">Selecione um arquivo em pdf</p>
+            <input
+              onChange={(e) => {
+                if (e.target.files) {
+                  setPdf(e.target.files[0]);
+                }
+              }}
+              type="file"
+              accept=".pdf"
+              className="block w-full text-sm text-gray-300
       file:mr-4 file:py-2 file:px-4
       file:rounded-lg file:border-0
       file:bg-[#1a1a1a] file:text-white 
      file:cursor-pointer
       cursor-pointer rouded-lg mt-2 "
-          />
+            />
+            <p className="text-xs mt-4">ou digite o título do assunto :</p>
+            <input
+              value={pdf}
+              onChange={(e) => setPdf(e.target.value)}
+              placeholder="Difite o título do assunto"
+              className="input outline-none
+             bg-[#1a1a1a] mt-2 p-2 text-xs "
+              type="text"
+            />
+          </div>
 
           <p className="text-xs mt-8">Selecione a difuldade</p>
           <select
